37f99bbdc6135f78f73530c8e568079dde70269b
Thisants <thiagsants21@gmail.com>
2026-02-08 20:55:28 -0300

    0.0.3
    implementação dar dark theme e light theme

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+const switcher =  document.querySelector('.btn');
+switcher.addEventListener('click', function() {
+
+    document.body.classList.toggle('dark-theme');var className = document.body.className;
+    if(className == "dark-theme"){
+        this.textContent = "dark";
+    } else {
+        this.textContent = "light";
+    }
+    }   
+);
